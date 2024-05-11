b138e55a617991d9f16b5b03cdf98299c8cb9533
Ng Heng Khai <nhkhai@gmail.com>
2024-05-12 01:02:47 +0800

    Update App.js
    Updated sample code.

--- MyApp/App.js
+++ MyApp/App.js
@@ -1,11 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+// import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet } from "react-native";
+import React, { Component } from "react";
+import { View, TextField, Text, Button } from "react-native-ui-lib";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    // <View style={styles.container}>
+    //   <Text>Open up App.js to start working on your app!</Text>
+    //   <StatusBar style="auto" />
+    // </View>
+    <View flex paddingH-25 paddingT-120>
+      <Text blue50 text20>
+        Welcome
+      </Text>
+      <TextField text50 placeholder="username" grey10 />
+      <TextField text50 placeholder="password" secureTextEntry grey10 />
+      <View marginT-100 center>
+        <Button text70 white background-orange30 label="Login" />
+        <Button link text70 orange30 label="Sign Up" marginT-20 />
+      </View>
     </View>
   );
 }
@@ -13,8 +27,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
